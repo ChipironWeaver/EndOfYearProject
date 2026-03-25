fa24a26a6667a79696342b24c59b712e4d287606
ChipironWeaver <lowen.tisserand@gmail.com>
2026-03-25 16:58:33 +0100

    [Fix]

--- Assets/Scripts/CameraPivotRotation.cs
+++ Assets/Scripts/CameraPivotRotation.cs
@@ -3,12 +3,14 @@ using UnityEngine.InputSystem;
 
 public class CameraPivotRotation : MonoBehaviour
 {
-    [SerializeField] Transform pivot;
+    [SerializeField] Transform player;
     [SerializeField] private float sensitivity;
+    [SerializeField] private float distance;
 
     private void OnLook(InputValue input)
     {
-        float mouse = input.Get<Vector2>().x;
-        pivot.Rotate(new Vector3(0,-mouse*sensitivity,0));
+        float mouse = input.Get<Vector2>().y;
+        transform.Rotate(new Vector3(-mouse*sensitivity,0,0));
+
     }
 }
--- Assets/Scripts/PlayerMovementController.cs
+++ Assets/Scripts/PlayerMovementController.cs
@@ -3,19 +3,20 @@ using UnityEngine.InputSystem;
 
 public class PlayerMovementController : MonoBehaviour
 {
+    [SerializeField] private float sensitivity;
     public float moveSpeed = 1;
     public float gravityForce = -9.8f;
-    public bool isGrounded = false;
+    public bool isGrounded;
     public float jumpForce;
     
-    private Rigidbody _rb;
     private Vector3 _move = Vector3.zero;
     private CharacterController _characterController;
     private float _verticalVelocity;
+    private Transform _transform;
     void Start()
     {
-        _rb = GetComponent<Rigidbody>();
         _characterController = GetComponent<CharacterController>();
+        _transform = GetComponent<Transform>();
     }
     
     void Update()
@@ -34,6 +35,12 @@ public class PlayerMovementController : MonoBehaviour
         {
             _verticalVelocity = Mathf.Sqrt(jumpForce * -2f * gravityForce);
         }
+    }
+    
+    private void OnLook(InputValue input)
+    {
+        float mouse = input.Get<Vector2>().x;
+        _transform.Rotate(new Vector3(0,-mouse*sensitivity,0));
     }
     
     void MovementUpdate()
