13f463dcd3808801b5467c57b6aa4a03673377b9
ChipironWeaver <lowen.tisserand@gmail.com>
2026-03-25 17:19:10 +0100

    [WIP] wip need clamp angle

--- Assets/Scripts/CameraPivotRotation.cs
+++ Assets/Scripts/CameraPivotRotation.cs
@@ -3,14 +3,19 @@ using UnityEngine.InputSystem;
 
 public class CameraPivotRotation : MonoBehaviour
 {
-    [SerializeField] Transform player;
     [SerializeField] private float sensitivity;
-    [SerializeField] private float distance;
+    [Range(1,10)][SerializeField] private float distance;
+    [SerializeField] private Transform child;
 
+    private void Update()
+    {
+        child.localPosition =new Vector3(0,0,-distance) ;
+    }
+    
     private void OnLook(InputValue input)
     {
         float mouse = input.Get<Vector2>().y;
         transform.Rotate(new Vector3(-mouse*sensitivity,0,0));
-
+        
     }
 }
