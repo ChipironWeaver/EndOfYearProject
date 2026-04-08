d3110f8f09c5aafc77d724b5903db3c9200f42b4
ChipironWeaver <lowen.tisserand@gmail.com>
2026-04-08 17:02:00 +0200

    Enemy spawner wip

---
+++ Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,8 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    public List<Transform> spawnPoints;
+    public List<GameObject> enemies;
+}
